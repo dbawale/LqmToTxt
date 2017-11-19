48803c6bc8acd03fb1ee03df4451ad15e3e3c381
Deven Bawale <dbawale@pdx.edu>
2017-11-18 21:23:23 -0800

    Added maven
    Added some method and a test to make sure maven works

--- src/lqmtotxt/Main.java
+++
@@ -1,8 +0,0 @@
-package lqmtotxt;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Test");
-    }
-}
---
+++ src/main/java/com/devenbawale/lqmtotxt/LqmToTxtApplication.java
@@ -0,0 +1,12 @@
+package com.devenbawale.lqmtotxt;
+
+public class LqmToTxtApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+
+    public String integerToString(int integer) {
+        return String.valueOf(integer);
+    }
+}
