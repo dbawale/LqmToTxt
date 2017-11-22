5f78bdaee29d2e8c25bae6499ba50cd659f4b671
Deven Bawale <dbawale@pdx.edu>
2017-11-21 22:12:14 -0800

    Added ability to check if less than one argument is passed

--- src/main/java/com/devenbawale/lqmtotxt/LqmToTxtApplication.java
+++ src/main/java/com/devenbawale/lqmtotxt/LqmToTxtApplication.java
@@ -3,6 +3,12 @@ package com.devenbawale.lqmtotxt;
 public class LqmToTxtApplication {
 
     public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.out.println("Too few arguments\n. Usage: lqmtotxtapplication <filename.lqm>");
+            return;
+        }
+
         System.out.println("Test");
     }
 
--- src/test/java/com/devenbawale/lqmtotxt/LqmToTxtApplicationTest.java
+++ src/test/java/com/devenbawale/lqmtotxt/LqmToTxtApplicationTest.java
@@ -9,10 +9,7 @@ import java.io.PrintStream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNot.not;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.spy;
 
 public class LqmToTxtApplicationTest {
@@ -35,9 +32,23 @@ public class LqmToTxtApplicationTest {
         //Execute the SUT
         lqmToTxtApplication.main(args);
 
-        //Assert
-        assertEquals("Test\r\n", bytes.toString());
+        //Validation
+        assertThat(bytes.toString(), is(equalTo("Test\r\n")));
+    }
 
+    @Test
+    public void testMain_tooFewArguments() throws Exception {
+        //Setup the fixture
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        LqmToTxtApplication lqmToTxtApplication = spy(new LqmToTxtApplication());
+        System.setOut(new PrintStream(bytes));
+        String[] args = new String[0];
+
+        //Execute the SUT
+        lqmToTxtApplication.main(args);
+
+        //Validation
+        assertThat(bytes.toString(), is(equalTo("Too few arguments\n. Usage: lqmtotxtapplication <filename.lqm>\r\n")));
     }
 
     @After
